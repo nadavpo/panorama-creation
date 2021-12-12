fix normalization and more cosmetic fixs

main.py
@@ -1,4 +1,5 @@
 import time
+import cv2
 import scipy.io
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -7,14 +8,6 @@ from cv2 import resize, INTER_CUBIC
 from matplotlib.patches import Circle
 
 from ex1_student_solution import Solution
-
-
-##########################################################
-# Don't forget to fill in your IDs!!!
-# students' IDs:
-ID1 = '123456789'
-ID2 = '987654321'
-##########################################################
 
 
 def tic():
@@ -50,87 +43,105 @@ def main():
     # Compute naive homography
     tt = time.time()
     naive_homography = solution.compute_homography_naive(match_p_src,
-                                                        match_p_dst)
+                                                         match_p_dst)
     print('Naive Homography {:5.4f} sec'.format(toc(tt)))
     print(naive_homography)
 
-    #Plot naive homography with forward mapping, slow implementation
+    # Plot naive homography with forward mapping, slow implementation
     tt = time.time()
     transformed_image = solution.compute_forward_homography_slow(
-       homography=naive_homography,
-       src_image=src_img,
-       dst_image_shape=dst_img.shape)
+        homography=naive_homography,
+        src_image=src_img,
+        dst_image_shape=dst_img.shape)
 
-    print('Naive Homography Slow computation takes {:5.4f} sec'.format(toc(tt)))
+    print(
+        'Naive Homography Slow computation takes {:5.4f} sec'.format(toc(tt)))
     plt.figure()
     forward_panorama_slow_plot = plt.imshow(transformed_image)
     plt.title('Forward Homography Slow implementation')
-    #plt.show()
+    # plt.show()
 
-    #Plot naive homography with forward mapping, fast implementation
+    # Plot naive homography with forward mapping, fast implementation
     tt = time.time()
     transformed_image_fast = solution.compute_forward_homography_fast(
-       homography=naive_homography,
-       src_image=src_img,
-       dst_image_shape=dst_img.shape)
+        homography=naive_homography,
+        src_image=src_img,
+        dst_image_shape=dst_img.shape)
 
-    print('Naive Homography Fast computation takes {:5.4f} sec'.format(toc(tt)))
+    print(
+        'Naive Homography Fast computation takes {:5.4f} sec'.format(toc(tt)))
     plt.figure()
     forward_panorama_fast_plot = plt.imshow(transformed_image_fast)
     plt.title('Forward Homography Fast implementation')
-    #plt.show()
+    # plt.show()
 
-    #loading data with imperfect matches
+    # loading data with imperfect matches
     src_img, dst_img, match_p_src, match_p_dst = load_data(False)
 
-    #Compute naive homography
+    # Compute naive homography
     tt = time.time()
     naive_homography = solution.compute_homography_naive(match_p_src,
-                                                        match_p_dst)
+                                                         match_p_dst)
     print('Naive Homography for imperfect matches {:5.4f} sec'.format(toc(tt)))
     print(naive_homography)
 
-    #Plot naive homography with forward mapping, fast implementation for
-    #imperfect matches
+    # Plot naive homography with forward mapping, fast implementation for
+    # imperfect matches
     tt = time.time()
     transformed_image_fast = solution.compute_forward_homography_fast(
-       homography=naive_homography,
-       src_image=src_img,
-       dst_image_shape=dst_img.shape)
+        homography=naive_homography,
+        src_image=src_img,
+        dst_image_shape=dst_img.shape)
 
     print('Naive Homography Fast computation for imperfect matches takes '
-         '{:5.4f} sec'.format(toc(tt)))
+          '{:5.4f} sec'.format(toc(tt)))
     plt.figure()
-    forward_panorama_imperfect_matches_plot = plt.imshow(transformed_image_fast)
+    forward_panorama_imperfect_matches_plot = plt.imshow(
+        transformed_image_fast)
     plt.title('Forward Panorama imperfect matches')
-    #plt.show()
+    # plt.show()
 
-   # Test naive homography
+    # Test naive homography
     tt = time.time()
     fit_percent, dist_mse = solution.test_homography(naive_homography,
-                                                    match_p_src,
-                                                    match_p_dst,
-                                                    max_err)
+                                                     match_p_src,
+                                                     match_p_dst,
+                                                     max_err)
     print('Naive Homography Test {:5.4f} sec'.format(toc(tt)))
     print([fit_percent, dist_mse])
 
-    #Compute RANSAC homography
+    # Compute RANSAC homography
     tt = tic()
     ransac_homography = solution.compute_homography(match_p_src,
-                                                   match_p_dst,
-                                                   inliers_percent,
-                                                   max_err)
+                                                    match_p_dst,
+                                                    inliers_percent,
+                                                    max_err)
     print('RANSAC Homography {:5.4f} sec'.format(toc(tt)))
     print(ransac_homography)
 
-    #Test RANSAC homography
+    # Test RANSAC homography
     tt = tic()
     fit_percent, dist_mse = solution.test_homography(ransac_homography,
-                                                    match_p_src,
-                                                    match_p_dst,
-                                                    max_err)
+                                                     match_p_src,
+                                                     match_p_dst,
+                                                     max_err)
     print('RANSAC Homography Test {:5.4f} sec'.format(toc(tt)))
     print([fit_percent, dist_mse])
+
+    # Plot naive  RANSAC homography with forward mapping
+    tt = time.time()
+    transformed_image_fast_RANSAC = solution.compute_forward_homography_fast(
+        homography=ransac_homography,
+        src_image=src_img,
+        dst_image_shape=dst_img.shape)
+
+    print('Naive Homography Fast computation for imperfect matches after RANSAC takes '
+          '{:5.4f} sec'.format(toc(tt)))
+    plt.figure()
+    forward_panorama_imperfect_matches_plot = plt.imshow(
+        transformed_image_fast_RANSAC)
+    plt.title('Forward Panorama RANSAC imperfect matches')
+    # plt.show()
 
     # Build panorama
     tt = tic()
@@ -154,7 +165,7 @@ def your_images_loader():
     src_img_test = mpimg.imread('src_test.jpg')
     dst_img_test = mpimg.imread('dst_test.jpg')
 
-    DECIMATION_FACTOR = 5.0
+    DECIMATION_FACTOR = 1
     src_img_test = resize(src_img_test,
                           dsize=(int(src_img_test.shape[1]/DECIMATION_FACTOR),
                                  int(src_img_test.shape[0]/DECIMATION_FACTOR)),
@@ -175,17 +186,17 @@ def your_images_loader():
 
 
 def your_images_main():
+    print('starting your images')
     solution = Solution()
     # Student Files
-    # first run "create_matching_points.py" with your own images to create a mat
-    # file with the matching coordinates.
-    max_err = 25  # <<<<< YOU MAY CHANGE THIS
-    inliers_percent = 0.8  # <<<<< YOU MAY CHANGE THIS
+
+    max_err = 25  
+    inliers_percent = 0.8 
 
     src_img_test, dst_img_test, match_p_src, match_p_dst = your_images_loader()
     homography = solution.compute_homography(match_p_src, match_p_dst,
                                              inliers_percent,
-                                             max_err=25)
+                                             max_err=max_err)
     img = solution.compute_forward_homography_fast(
         homography=homography,
         src_image=src_img_test,
@@ -201,8 +212,8 @@ def your_images_main():
                                                       max_err=25)
     img = solution.compute_backward_mapping(
         backward_projective_homography=backward_homography,
-                                      src_image=src_img_test,
-                                      dst_image_shape=dst_img_test.shape)
+        src_image=src_img_test,
+        dst_image_shape=dst_img_test.shape)
     plt.figure()
     import numpy as np
     student_backward_warp_img = plt.imshow(img.astype(np.uint8))